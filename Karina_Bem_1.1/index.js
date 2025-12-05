060167b915f844941d3b1b593a951e1152f5f7ad
kamawebdev13 <karinadatanalyst13@alumnos.cei.es>
2025-12-05 20:41:56 +0100

    Js and css done

--- Karina_Bem_1.1/index.js
+++ Karina_Bem_1.1/index.js
@@ -1,2 +1,17 @@
 
 
+const headerBurger = document.querySelector(".header__burger")
+
+const menuResponsive = document.querySelector(".header__responsive__menu")
+
+
+
+headerBurger.addEventListener("click", () => {
+
+    headerBurger.classList.toggle("open")
+
+    menuResponsive.classList.toggle("open")
+
+
+
+})
